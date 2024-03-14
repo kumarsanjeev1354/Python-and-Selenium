27e38ec15e907bd7599078cfdc235ea4fbb1ba2a
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2024-03-15 00:21:30 +0530

    Second Selenium program
    Signed-off-by: kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>

--- First Selenium Project.py
+++ First Selenium Project.py
@@ -1,6 +1,5 @@
 print("hello test1")
 from selenium import webdriver
-
 driver=webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.google.com")
@@ -8,3 +7,5 @@ print("Application name is",driver.title)
 print("Current url is",driver.current_url)
 driver.quit()
 print("URL Execution End")
+new driver=driver.find_element(By.ID, "APjFqb")
+print("id is",new driver)
